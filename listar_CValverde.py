8f350c182f03d0a4d6b07cbff36c5fff8487555c
carlosvalv03 <94960852+carlosvalv03@users.noreply.github.com>
2022-03-10 09:23:15 +0100

    Add files via upload

--- listar_CValverde.py
+++ listar_CValverde.py
@@ -1,6 +1,6 @@
 #Este codigo esta hecho por Carlos el dia 08/03/2022
 #Esta opción lista todos los nombres y numeros que has añadido a la agenda 
-def listar(agenda):
+def listar_CValverde(agenda):
     for nombre, numero in agenda.items():
                 print("----------------------")
                 print("|        Listado      |")  
